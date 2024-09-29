790c87e3330f80faa948a47943226dd2fde08248
Rkislam3390990 <ri3390990@gmail.com>
2024-09-29 21:45:11 +0600

    first commit

---
+++ js/tailwind.config.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        // clifford: '#da373d',
+      },
+      backgroundImage: {
+        'hero-pattern': "url('images/back.jpeg')",
+      },
+      'display': ['Poppins'],
+      
+    }
+  }
+}
